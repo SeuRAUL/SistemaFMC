105c6c0596deab3de997208c8915b4d16bc55743
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-06-04 08:54:36 -0300

    aplicação startada com home/index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaFMC::Application.config.secret_token = 'cc0ba7e8198e8c501e51d2df6ee56f3867491dae4beac082bd51e7518e8422759ba8c87653c1e43c771af970f48c8eda5cd65a30b5b8a08dbe936c202c8a3841'
